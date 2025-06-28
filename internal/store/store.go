418396ade7392dba9ee8d7a2381c27e7aac6864e
roi <roibuenaventura05@gmail.com>
2025-06-28 11:09:22 +0800

    add write ahead log functionality

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,14 +1,58 @@
 package store
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"sync"
+)
 
 type Store struct {
 	mu   sync.RWMutex
 	data map[string]string
+	log  *os.File
 }
 
+// Initialize the store and open the WAL file
 func NewStore() *Store {
-	return &Store{data: make(map[string]string)}
+	logFile, err := os.OpenFile("store.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	s := &Store{
+		data: make(map[string]string),
+		log:  logFile,
+	}
+
+	s.replayLog()
+	return s
+}
+
+// Write SET operations to the WAL
+func (s *Store) Set(key, value string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	entry := fmt.Sprintf("SET %s %s\n", key, value)
+	if _, err := s.log.WriteString(entry); err != nil {
+		fmt.Println("log write error:", err)
+	}
+
+	s.data[key] = value
+}
+
+// Replay WAL on startup to rebuild the in-memory store
+func (s *Store) replayLog() {
+	buf := make([]byte, 4096)
+	n, _ := s.log.Read(buf)
+	lines := string(buf[:n])
+	for _, line := range splitLines(lines) {
+		var cmd, key, val string
+		fmt.Sscanf(line, "%s %s %s", &cmd, &key, &val)
+		if cmd == "SET" {
+			s.data[key] = val
+		}
+	}
 }
 
 func (s *Store) Get(key string) (string, bool) {
@@ -18,8 +62,19 @@ func (s *Store) Get(key string) (string, bool) {
 	return val, ok
 }
 
-func (s *Store) Set(key, value string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.data[key] = value
+func splitLines(s string) []string {
+	var lines []string
+	var curr string
+	for _, c := range s {
+		if c == '\n' {
+			lines = append(lines, curr)
+			curr = ""
+		} else {
+			curr += string(c)
+		}
+	}
+	if curr != "" {
+		lines = append(lines, curr)
+	}
+	return lines
 }
